0f59992e7a7bb93a950d8a63af86e3ed70a37ce2
Lindsay Brown <lindsayluwho@gmail.com>
2023-01-17 00:34:51 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-//import Helmet from 'react-helmet'
+import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import '../assets/scss/main.scss'
@@ -38,15 +38,15 @@ const Layout = ({ children, location }) => {
       `}
       render={data => (
         <>
-         // <Helmet
-          //  title={data.site.siteMetadata.title}
-          //  meta={[
-         //     { name: 'description', content: 'Sample' },
-         //     { name: 'keywords', content: 'sample, something' },
-        //    ]}
-          //>
-            //<html lang="en" />
-          //</Helmet>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' },
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
           {content}
         </>
       )}
